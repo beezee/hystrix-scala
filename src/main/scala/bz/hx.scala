a2db15333f36aed5d566466f304bbc437d618ff0
Brian Zeligson <bzeligson@localytics.com>
2015-11-13 10:22:55 -0500

    make throwables accurate and useful

--- src/main/scala/bz/hx.scala
+++ src/main/scala/bz/hx.scala
@@ -1,6 +1,5 @@
 package bz
 
-import HxShortCircuit._
 import com.netflix.hystrix.{HystrixCommand, HystrixCommandGroupKey}
 import com.netflix.hystrix.HystrixCommandKey
 import com.netflix.hystrix.HystrixCommand.Setter
@@ -9,18 +8,32 @@ import scalaz.Unapply
 import scalaz.syntax.either._
 import scalaz.syntax.std.boolean._
 
-object HxShortCircuit {
-  class HxShortCircuit extends Throwable
-}
-
 trait HxInterface[M[_]] {
 
-  def hxC[A](s: Setter)(
-                      fn: () => A): HystrixCommand[M[A]]
+  def hxC[A](s: Setter)(fn: () => A): HxCommand[M[A]]
 
-  def mHx[A](s: Setter)(
-                      fn: () => A): M[A] =
+  def mHx[A](s: Setter)(fn: () => A): M[A] =
     hxC(s)(fn).execute()
+}
+
+abstract class HxCommand[A](s: Setter) extends HystrixCommand[A](s) {
+  class HxShortCircuit(m: String) extends Throwable(m)
+  class HxMaxedOut(m: String) extends Throwable(m)
+  class HxTimedOut(m: String) extends Throwable(m)
+  class HxMaxedOutTimedOut(m: String) extends Throwable(m)
+
+  def getThr: Throwable =
+    (isResponseShortCircuited, isResponseRejected,
+      isResponseTimedOut) match {
+      case (false, false, false) =>
+            Option(getFailedExecutionException)
+              .getOrElse(new Throwable("unknown-hx-failure"))
+      case (true, _, _) => new HxShortCircuit("hx-short-circuited")
+      case (false, true, false) => new HxMaxedOut("hx-maxed-out")
+      case (false, false, true) => new HxTimedOut("hx-timed-out")
+      case (false, true,true) =>
+        new HxMaxedOutTimedOut("hx-maxed-out-timed-out")
+    }
 }
 
 object HxInterfaces {
@@ -28,8 +41,8 @@ object HxInterfaces {
   implicit val optionHx = new HxInterface[Option] {
 
     def hxC[A](s: Setter)(
-                        fn: () => A): HystrixCommand[Option[A]] =
-      new HystrixCommand[Option[A]](s) {
+                        fn: () => A): HxCommand[Option[A]] =
+      new HxCommand[Option[A]](s) {
         override def run(): Option[A] = Some(fn())
         override def getFallback(): Option[A] = None
       }
@@ -39,17 +52,18 @@ object HxInterfaces {
   implicit val djHx = new HxInterface[THD] {
 
       def hxC[A](s: Setter)(
-                          fn: () => A): HystrixCommand[THD[A]] =
-        new HystrixCommand[\/[Throwable, A]](s) {
+                          fn: () => A): HxCommand[THD[A]] =
+        new HxCommand[\/[Throwable, A]](s) {
           override def run(): \/[Throwable, A] =
             fn().right[Throwable]
           override def getFallback(): \/[Throwable, A] =
-            isCircuitBreakerOpen.fold(
-              (new HxShortCircuit),
-            Option(getFailedExecutionException)
-              .getOrElse(new Throwable("unknown-hx-failure")))
-            .left[A]
+            getThr.left[A]
         }
+
+      override def mHx[A](s: Setter)(fn: () => A): THD[A] =  {
+        val c = hxC(s)(fn)
+        c.execute().leftMap(_ => c.getThr)
+      }
   }
 }
 
@@ -60,9 +74,9 @@ object HxControl {
     extends HxResult[A]
   case class HxOk[A](get: A) extends HxResult[A]
 
-  def controlInterface[A](s: Setter)(
-                   fn: () => HxResult[A]): HxResult[A] =
-    new HystrixCommand[HxResult[A]](s) {
+  def controlCommand[A](s: Setter)(
+                   fn: () => HxResult[A]): HxCommand[HxResult[A]] =
+    new HxCommand[HxResult[A]](s) {
       @volatile private var res: HxResult[A] = null
 
       override def run(): HxResult[A] = {
@@ -81,17 +95,23 @@ object HxControl {
       override def getFallback(): HxResult[A] =
         Option(res).map { r =>
           r match {
-            case HxFail(a, _) => {
-              val t = isCircuitBreakerOpen.fold(
-                        (new HxShortCircuit),
-                      Option(getFailedExecutionException)
-                        .getOrElse(new Throwable("unknown-hx-failure")))
-              HxFail(a, Some(t))
-            }
+            case HxFail(a, _) => HxFail(a, Some(getThr))
             case k: HxOk[A] => k
           }
-        }.getOrElse(HxFail(th = Some(new HxShortCircuit)))
-    }.execute()
+        }.getOrElse(HxFail(None, Some(getThr)))
+    }
+
+    def controlInterface[A](s: Setter)(fn: () => HxResult[A]):
+    HxResult[A] = {
+      val c = controlCommand(s)(fn)
+      val r = c.execute()
+      r match {
+        case o@HxFail(a, t) =>
+          t.map(_ => HxFail(a, Some(c.getThr))).getOrElse(o)
+        case _ => r
+      }
+    }
+
 }
 
 object HX {
