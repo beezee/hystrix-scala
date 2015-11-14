4021f204b5166c40a0f9d22a6a7dd3aea3e4d0e9
Brian Zeligson <bzeligson@localytics.com>
2015-11-13 23:22:18 -0500

    scaladoc, fix liftHx, clearer use of dummy fallback on instances with throwables

--- src/main/scala/bz/syntax/HxSyntax.scala
+++ src/main/scala/bz/syntax/HxSyntax.scala
@@ -5,20 +5,48 @@ import bz.HxInterface
 import com.netflix.hystrix.HystrixCommand.Setter
 import scalaz.\/
 
+/**
+ * Implicit classes decorating functions and Setters
+ * with methods for execution inside of a HystrixCommand
+ */
 object hx {
 
-  implicit class HxLift[M[_], A](fn: () => A)(
-                                implicit hxi: HxInterface[M]) {
-    def liftHx(s: Setter): M[A] =
+  /**
+   * Implicit class decorating functions of arity 0
+   */
+  implicit class HxLift[A](fn: () => A) {
+
+    /**
+     * Lift a function of arity 0 into a
+     * HystrixCommand and execute using
+     * the given Setter
+     */
+    def liftHx[M[_]](s: Setter)(implicit hxi: HxInterface[M]): M[A] =
       hxi.mHx(s)(fn)
   }
 
+  /**
+   * Implicit class decorating a HystrixCommand.Setter
+   */
   implicit class GKLift[A](s: Setter){
+
+    /**
+     * Lift a Setter into a HystrixCommand and
+     * execute using the given () => A
+     */
     def run[M[_]](fn: () => A)(implicit hxi: HxInterface[M]): M[A] =
       hxi.mHx(s)(fn)
   }
 
-  implicit class DjLift(s: Setter){
+  /**
+   * Implicit class decorating a HystrixCommand.Setter
+   */
+  implicit class ControlLift(s: Setter){
+
+    /**
+     * Lift a Setter into a HystrixCommand and
+     * execute using the given () => HxResult[A]
+     */
     def runC[A](fn: () => HxResult[A]): HxResult[A] =
       controlInterface(s)(fn)
   }
